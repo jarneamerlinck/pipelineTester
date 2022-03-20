2aaad1590b3809067633b5eb31b658363916bc82
jarneamerlinck <jarneamerlinck@gmail.com>
2022-03-20 15:13:06 +0100

    Updating from tutorial

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Imports
+import math
+
+# Start
+print("It can work first try")
+
+
+def adding(a, b):
+    return a+b
---
+++ tests/test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Imports
+import math
+import unittests
+from main import adding
+# Start
+
+class Maintester(unittests.Testcase):
+    def setUp(self):
+        pass
+    def testAdding(self):
+        self.assertEqual(adding(3, 5), 8)
+
+
+
+if __name__= '__main__':
+    unittest.main()
